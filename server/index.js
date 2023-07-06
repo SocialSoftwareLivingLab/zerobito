7f16d00dee8a9dcf635c50621f042c570a789e42
Harlock221B <gabrielferreira221b@gmail.com>
2023-07-06 16:41:38 -0300

    Upando primeira versão zerobito

---
+++ server/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Aranha32342#",
+  database: "banco",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length == 0) {
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        db.query(
+          "INSERT INTO usuarios (email, password) VALUE (?,?)",
+          [email, hash],
+          (error, response) => {
+            if (err) {
+              res.send(err);
+            }
+
+            res.send({ msg: "Usuário cadastrado com sucesso" });
+          }
+        );
+      });
+    } else {
+      res.send({ msg: "Email já cadastrado" });
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    if (result.length > 0) {
+      bcrypt.compare(password, result[0].password, (error, response) => {
+        if (error) {
+          res.send(error);
+        }
+        if (response) {
+          res.send({ msg: "Usuário logado" });
+        } else {
+          res.send({ msg: "Senha incorreta" });
+        }
+      });
+    } else {
+      res.send({ msg: "Usuário não registrado!" });
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("rodando na porta 3001");
+});
