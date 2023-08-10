e3c2f7a512dc123d0b70bc0bb2fcff3cd4a52566
Harlock221B <gabrielferreira221b@gmail.com>
2023-08-10 12:30:52 -0300

    Landing Page update

---
+++ src/components/Menu/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+import React from 'react';
+
+
+function Menu() {
+    return (
+        <div className="Menu">
+            
+        </div>
+    );
+  };
+  
+export default Menu;
