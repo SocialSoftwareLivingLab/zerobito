54b764997880c52a1971dece7da7508a7974ca38
Leonardo Braz <lhleonardo05@gmail.com>
2024-01-22 14:25:54 -0300

    configura env para executar localmente sem perfil

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://143.106.73.48:3001",
+    // TODO: adicionar ao .env do projeto
+    baseURL: "http://localhost:3001",
 });
 
 export default api;
