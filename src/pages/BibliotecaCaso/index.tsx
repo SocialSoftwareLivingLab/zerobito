1cfb497516a616dd85031c64a54c7a228f930dfc
Henrique <h247165@dac.unicamp.br>
2024-02-08 14:04:40 -0300

    adicionado layout de casos e botoes de pesquisa (ainda sem utilidade aplicada)

--- src/pages/BibliotecaCaso/index.tsx
+++ src/pages/BibliotecaCaso/index.tsx
@@ -15,7 +15,46 @@ function BibliotecaCasos(){
                 </div>
             </div>
             <div className='search-container'>
-            <input type="text" value="digite alguma coisa"/>
+                <input type="text" value="digite alguma coisa"/>
+                <button className='search-container-pesquisar' type="submit" >Pesquisar</button>
+                <button className="search-container-observatorio" type="submit" >Observatório ZerÓbito</button>
+            </div>
+            <div className='casos-acompanhados'>
+                <div className='casos-acompanhados-header'>
+                    <h1>Casos que você acompanha</h1>
+                </div>
+                <div className='indices-casos'>
+                    <input type="checkbox"/>
+                    <label>Data</label>
+                    <label>Denúncia</label>
+                    <label>Causa</label>
+                    <label>Gravidade</label>
+                    <label>Status</label>
+                    <label>Cidade</label>
+                    <label>Região</label>
+                    <button type='submit'>Ver release</button>
+                </div>
+                <div className='lista-casos-acompanhados'>
+
+                </div>
+            </div>
+            <div className='casos-acompanhados'>
+                <div className='casos-acompanhados-header'>
+                    <h1>Todos os casos</h1>
+                </div>
+                <div className='indices-casos'>
+                    <input type="checkbox"/>
+                    <label>Data</label>
+                    <label>Denúncia</label>
+                    <label>Causa</label>
+                    <label>Gravidade</label>
+                    <label>Status</label>
+                    <label>Cidade</label>
+                    <label>Região</label>
+                </div>
+                <div className='lista-casos-acompanhados'>
+
+                </div>
             </div>
         </div>
     );
