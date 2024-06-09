2f56ce4327e916515ce1f206ad99b46f1a7ee274
Leonardo Braz <lhleonardo05@gmail.com>
2024-06-09 00:20:48 -0300

    monta todo o header do caso

--- src/components/Caso/CasoInfo/index.tsx
+++ src/components/Caso/CasoInfo/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaBell, FaFileAlt } from 'react-icons/fa';
 import { MdOutlineDateRange } from 'react-icons/md';
 import { CasoInfoContainer } from './styles';
+import CasoInfoItem from './CasoInfoItem';
 
 export function CasoInfo() {
     const dateFormat = Intl.DateTimeFormat('pt-br');
@@ -10,22 +11,24 @@ export function CasoInfo() {
         id: 1,
         dataCriacao: new Date()
     };
+
     return (
         <CasoInfoContainer>
-            <header>
-                <label>
-                    <FaFileAlt /> Código do caso:
-                </label>
-                <span>{String(caso.id).padStart(5, '0')}</span>
-                <label>
-                    <MdOutlineDateRange /> Data de Denúncia:
-                </label>
-                <span>{dateFormat.format(new Date(caso.dataCriacao))}</span>
-                <label>
-                    <FaBell /> Status de notificação obrigatório:
-                </label>
-                <span>Aguardando Análise</span>
-            </header>
+            <CasoInfoItem
+                icone={<FaFileAlt />}
+                label="Código do caso"
+                valor={String(caso.id).padStart(5, '0')}
+            />
+            <CasoInfoItem
+                icone={<MdOutlineDateRange />}
+                label="Data de Denúncia"
+                valor={dateFormat.format(new Date(caso.dataCriacao))}
+            />
+            <CasoInfoItem
+                icone={<FaBell />}
+                label="Status de notificação obrigatório"
+                valor="Aguardando Análise"
+            />
         </CasoInfoContainer>
     );
 }
