43f6b40e46ac7e6811e58d1426f6397579c185b0
Leonardo Braz <lhb.consultoria@wefin.com.br>
2024-06-25 01:11:20 -0300

    melhora visualização do mapa

--- src/components/DossieCard/LocalizacaoCard/index.tsx
+++ src/components/DossieCard/LocalizacaoCard/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { FaLocationDot } from 'react-icons/fa6';
-import { LocalizacaoCardContainer } from './styles';
+import { LocalizacaoCardContainer, Metadado, Metadados } from './styles';
 
-import MapaGeografico, { MarcadorLocalizacaoMapa } from '../../ui/MapaGeografico';
 import { useCasoSelecionado } from '../../../contexts/caso-selecionado';
+import MapaGeografico, { MarcadorLocalizacaoMapa } from '../../ui/MapaGeografico';
 
 export default function LocalizacaoCard() {
+    const {
+        caso: { localizacao }
+    } = useCasoSelecionado();
+
     const marcadores: MarcadorLocalizacaoMapa[] = [
         {
             coordenada: [-22.9102, -47.060898],
@@ -29,7 +33,23 @@ export default function LocalizacaoCard() {
             <h3>
                 <FaLocationDot /> Localização
             </h3>
-            <MapaGeografico marcadores={marcadores} />
+            <div className="separador-localizacao">
+                <MapaGeografico marcadores={marcadores} />
+                <Metadados>
+                    <Metadado>
+                        <h4>Cidade</h4>
+                        <span>{localizacao.cidade}</span>
+                    </Metadado>
+                    <Metadado>
+                        <h4>Estado</h4>
+                        <span>{localizacao.estado}</span>
+                    </Metadado>
+                    <Metadado>
+                        <h4>Logradouro</h4>
+                        <span>{localizacao.logradouro}</span>
+                    </Metadado>
+                </Metadados>
+            </div>
         </LocalizacaoCardContainer>
     );
 }
--- src/components/DossieCard/LocalizacaoCard/styles.ts
+++ src/components/DossieCard/LocalizacaoCard/styles.ts
@@ -1,20 +1,46 @@
 import styled from 'styled-components';
 
 export const LocalizacaoCardContainer = styled.div`
-    height: 600px;
+    height: 400px;
 
     h3 {
         border-bottom: 1px solid #134780;
         padding-bottom: 15px;
     }
 
-    .leaflet-container {
+    .separador-localizacao {
         height: 100%;
-        border-radius: 0.5rem;
-        margin-top: 15px;
+        display: flex;
+        flex-direction: row;
+
+        .leaflet-container {
+            height: 100%;
+            border-radius: 0.5rem;
+            margin-top: 15px;
+            width: 50%;
+        }
+
+        .leaflet-control-attribution {
+            display: inline;
+        }
+    }
+`;
+
+export const Metadados = styled.div`
+    margin-left: 16px;
+`;
+
+export const Metadado = styled.div`
+    margin-top: 16px;
+
+    h4 {
+        color: #134780;
+        font-weight: bold;
+        font-size: 20px;
     }
 
-    .leaflet-control-attribution {
-        display: inline;
+    span {
+        font-size: 16px;
+        margin-top: 8px;
     }
 `;
